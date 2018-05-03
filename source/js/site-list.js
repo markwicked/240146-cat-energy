8d01eb9ea1c3372da293d1b3dd6165e1bf6bb4ae
tomhet <markwicked@gmail.com>
2018-05-03 18:04:29 +0500

    Мелкие исправления

--- source/js/site-list.js
+++ source/js/site-list.js
@@ -1,6 +1,9 @@
   var toggle = document.querySelector (".main-nav__toggle");
   var sitelist = document.querySelector(".site-list");
 
+  toggle.classList.remove("main-nav__toggle--close");
+  sitelist.classList.remove("site-list--no-js");
+
   toggle.addEventListener("click", function(event) {
     event.preventDefault();
     sitelist.classList.toggle("site-list--opened");
