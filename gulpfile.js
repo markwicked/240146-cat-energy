a4d09fe9b278168c1c7a3eb5c6553de30429ca45
tomhet <markwicked@gmail.com>
2018-05-06 18:47:48 +0500

    Мелкие исправления

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,6 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
     "source/js/**"
   ], {
     base: "source"
